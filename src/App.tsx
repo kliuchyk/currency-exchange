c8e8de3b12c102fb695a56e1c5896152e2e800ce
kliuchyk <kliuchyk@gmail.com>
2020-11-13 11:47:38 +0200

    add pages routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,49 @@
 import React from 'react';
 import AppWrapper from './containers/AppWrapper';
+import { Route, Switch, Link } from 'react-router-dom';
+import { Button, Layout } from 'antd';
+import { LeftOutlined, RightOutlined } from '@ant-design/icons';
+
+import CurrencyConverter from './containers/CurrencyConverter';
+import CurrenciesList from './containers/CurrenciesList';
 import './App.css';
 
 function App() {
   return (
     <AppWrapper>
-      <div className="App"></div>
+      <div className="App">
+        <Layout>
+          <Switch>
+            <Route exact path="/currencies">
+              <Layout.Header>
+                <Link to="/">
+                  <Button type="primary">
+                    <LeftOutlined />
+                    Back to Converter
+                  </Button>
+                </Link>
+              </Layout.Header>
+              <Layout.Content>
+                <CurrenciesList />
+              </Layout.Content>
+            </Route>
+
+            <Route exact path="/">
+              <Layout.Header>
+                <Link to="/currencies">
+                  <Button type="primary">
+                    <RightOutlined />
+                    Currencies
+                  </Button>
+                </Link>
+              </Layout.Header>
+              <Layout.Content>
+                <CurrencyConverter />
+              </Layout.Content>
+            </Route>
+          </Switch>
+        </Layout>
+      </div>
     </AppWrapper>
   );
 }
