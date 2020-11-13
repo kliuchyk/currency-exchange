e7285e48c298058d99f8be48a50df25c9b8ca6bb
kliuchyk <kliuchyk@gmail.com>
2020-11-13 11:48:50 +0200

    get currencies rates

---
+++ src/api.ts
@@ -0,0 +1,6 @@
+const BASE_API = process.env.REACT_APP_EXCHANGE_RATES_BASE_API;
+
+export const getCurrenciesRates = async (base: string = 'USD') => {
+  const response = await fetch(`${BASE_API}/latest?base=${base}`, { mode: 'cors' });
+  return await response.json();
+};
--- src/containers/CurrenciesList/index.tsx
+++ src/containers/CurrenciesList/index.tsx
@@ -1,5 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getCurrenciesRates } from '../../api';
 
 export default function CurrenciesList() {
-  return <div>Currencies List</div>;
+  const [rates, setRates] = useState([]);
+
+  useEffect(() => {
+    const fetchRates = async () => {
+      const { base, rates } = await getCurrenciesRates();
+      console.log(base);
+      console.log(Array.isArray(rates));
+      setRates(rates);
+    };
+    fetchRates();
+  }, []);
+
+  return (
+    <div>
+      <h3>Currencies Rates</h3>
+      {Object.keys(rates).map((itemKey, idx) => (
+        <div key={idx}>{itemKey}</div>
+      ))}
+    </div>
+  );
 }
