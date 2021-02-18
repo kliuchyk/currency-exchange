03808f5fc4da34d5476cd8d1db09368ae8086802
kliuchyk <kliuchyk@gmail.com>
2021-02-18 15:48:33 +0200

    fix styles for currencies page

--- src/pages/CurrenciesList/index.tsx
+++ src/pages/CurrenciesList/index.tsx
@@ -26,26 +26,28 @@ export default function CurrenciesList() {
 
   return (
     <div>
-      <h3>Currencies Rates</h3>
+      <Title>Currencies Rates</Title>
       <RatesList>
         {allIds.map((id) => {
           const currency = byId[id];
           const { origin, rate, isFavorite } = currency;
           return (
-            <CurrencyItem key={origin}>
-              <div className="item-desc">
-                <p>{origin}</p>
-                <span>{rate}</span>
-              </div>
-              <span
-                className="star"
-                onClick={() =>
-                  dispatch(toggleFavorite((origin as unknown) as string))
-                }
-              >
-                {isFavorite ? <StarFilled /> : <StarOutlined />}
-              </span>
-            </CurrencyItem>
+            <ListItem>
+              <CurrencyItem key={origin}>
+                <div className="item-desc">
+                  <p>{origin}</p>
+                  <span>{rate}</span>
+                </div>
+                <span
+                  className="star"
+                  onClick={() =>
+                    dispatch(toggleFavorite((origin as unknown) as string))
+                  }
+                >
+                  {isFavorite ? <StarFilled /> : <StarOutlined />}
+                </span>
+              </CurrencyItem>
+            </ListItem>
           );
         })}
       </RatesList>
@@ -53,28 +55,43 @@ export default function CurrenciesList() {
   );
 }
 
+const Title = styled.h2`
+  text-align: center;
+  font-family: 'Poppins', sans-serif;
+  font-weight: 500;
+  font-size: 28px;
+  color: #fff;
+`;
+
 const RatesList = styled.ul`
   padding: 0;
+  height: 600px;
+  -webkit-column-count: 3;
+  -moz-column-count: 3;
+  column-count: 3;
+  font-family: 'Poppins', sans-serif;
+`;
+
+const ListItem = styled.div`
+  display: inline-block;
 `;
 
 const CurrencyItem = styled.div`
   margin-bottom: 15px;
   padding: 0;
   margin: 10px auto;
-  width: 300px;
+  padding-right: 30px;
+  width: 120px;
   display: flex;
   justify-content: center;
   align-items: center;
   cursor: pointer;
   position: relative;
+  color: #fff;
 
   p {
     margin: 0;
     font-weight: bold;
-  }
-
-  span {
-    color: #888;
   }
 
   button {
